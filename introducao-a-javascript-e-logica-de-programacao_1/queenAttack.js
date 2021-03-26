cd3ad38b4ac734602978b1a32ed48eca809ab863
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-03-26 19:32:22 -0300

    Exercicio do AtaqueDaRainhaV1

--- introducao-a-javascript-e-logica-de-programacao_1/queenAttack.js
+++ introducao-a-javascript-e-logica-de-programacao_1/queenAttack.js
@@ -6,3 +6,50 @@ let opponentColumnPosition = 3;
 
 let sucessfullAttack = false;
 
+if (queenRowPosition === opponentRowPosition) {
+    sucessfullAttack = true;
+}
+else if (queenColumnPosition === opponentColumnPosition){
+    sucessfullAttack = true;
+}
+for (let index=1;index < 8 ;index+=1){
+    let currentQueenRowPosition = queenRowPosition - index;
+    let currentQueenColumnPosition = queenColumnPosition - index;
+    if(currentQueenRowPosition === 0 || currentQueenColumnPosition === 0){
+        break;
+    }
+    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentRowPosition){
+        sucessfullAttack = true;
+    }
+}
+for (let index=1;index < 8 ;index+=1){
+    let currentQueenRowPosition = queenRowPosition - index;
+    let currentQueenColumnPosition = queenColumnPosition + index;
+    if(currentQueenRowPosition === 0 || currentQueenColumnPosition < 8){
+        break;
+    }
+    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentRowPosition){
+        sucessfullAttack = true;
+    }
+}
+for (let index=1;index < 8 ;index+=1){
+    let currentQueenRowPosition = queenRowPosition + index;
+    let currentQueenColumnPosition = queenColumnPosition + index;
+    if(currentQueenRowPosition < 8 || currentQueenColumnPosition < 8){
+        break;
+    }
+    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentRowPosition){
+        sucessfullAttack = true;
+    }
+}
+for (let index=1;index < 8 ;index+=1){
+    let currentQueenRowPosition = queenRowPosition + index;
+    let currentQueenColumnPosition = queenColumnPosition - index;
+    if(currentQueenRowPosition < 8 || currentQueenColumnPosition === 0){
+        break;
+    }
+    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentRowPosition){
+        sucessfullAttack = true;
+    }
+}
+
