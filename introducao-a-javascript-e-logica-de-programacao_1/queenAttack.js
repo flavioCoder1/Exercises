fa0e910d3790e6d1b4c7015064cd80b90af310c1
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-03-26 20:15:08 -0300

    Exercicio Ataque da Rainha V2

--- introducao-a-javascript-e-logica-de-programacao_1/queenAttack.js
+++ introducao-a-javascript-e-logica-de-programacao_1/queenAttack.js
@@ -2,33 +2,34 @@ let queenRowPosition = 4;
 let queenColumnPosition = 4;
 
 let opponentRowPosition = 3;
-let opponentColumnPosition = 3;
+let opponentColumnPosition = 2;
 
 let sucessfullAttack = false;
 
 if (queenRowPosition === opponentRowPosition) {
     sucessfullAttack = true;
 }
-else if (queenColumnPosition === opponentColumnPosition){
+if (queenColumnPosition === opponentColumnPosition){
     sucessfullAttack = true;
 }
-for (let index=1;index < 8 ;index+=1){
+
+for (let index = 1;index < 8 ;index += 1){
     let currentQueenRowPosition = queenRowPosition - index;
     let currentQueenColumnPosition = queenColumnPosition - index;
     if(currentQueenRowPosition === 0 || currentQueenColumnPosition === 0){
         break;
     }
-    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentRowPosition){
+    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentColumnPosition){
         sucessfullAttack = true;
     }
 }
-for (let index=1;index < 8 ;index+=1){
+for (let index =1;index < 8 ; index+=1){
     let currentQueenRowPosition = queenRowPosition - index;
     let currentQueenColumnPosition = queenColumnPosition + index;
     if(currentQueenRowPosition === 0 || currentQueenColumnPosition < 8){
         break;
     }
-    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentRowPosition){
+    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentColumnPosition){
         sucessfullAttack = true;
     }
 }
@@ -38,7 +39,7 @@ for (let index=1;index < 8 ;index+=1){
     if(currentQueenRowPosition < 8 || currentQueenColumnPosition < 8){
         break;
     }
-    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentRowPosition){
+    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentColumnPosition){
         sucessfullAttack = true;
     }
 }
@@ -48,8 +49,13 @@ for (let index=1;index < 8 ;index+=1){
     if(currentQueenRowPosition < 8 || currentQueenColumnPosition === 0){
         break;
     }
-    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentRowPosition){
+    if( currentQueenRowPosition === opponentRowPosition && currentQueenColumnPosition === opponentColumnPosition){
         sucessfullAttack = true;
     }
 }
-
+if (sucessfullAttack === true){
+    console.log('Ataque da Rainha REALIZADO!!! :)')
+}
+else {
+    console.log('Ataque da Rainha NÃO foi efetuado :(')
+}
